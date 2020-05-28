4a24e1ba9b4bf841073b27a4023416fc3707214a
DontaH06 <64978806+dhatch06@users.noreply.github.com>
2020-05-28 13:47:04 -0400

    update optional get id

--- src/main/java/com/example/todos/todo/TodoController.java
+++ src/main/java/com/example/todos/todo/TodoController.java
@@ -29,7 +29,7 @@ public class TodoController{
     public Todo editTodo(@PathVariable long id, @RequestBody Todo todo) {
         Todo existingTodo = new Todo();
         try{
-            existingTodo = todoRepository.findById(id).orElseThrow();
+            existingTodo = todoRepository.findById(id).get();
             existingTodo.setTitle(todo.getTitle());
             existingTodo.setDescription(todo.getDescription());
             existingTodo.setComplete(todo.isComplete());
